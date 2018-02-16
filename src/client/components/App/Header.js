e75f28a94e017b22b232e4c3286465d4d56a47f3
Brandon Fizer <30492636+Track7Dev@users.noreply.github.com>
2018-02-16 02:17:54 -0500

    PR DISPLAY ALL REPOS

--- src/client/components/App/Header.js
+++ src/client/components/App/Header.js
@@ -54,7 +54,7 @@ class Header extends Component {
        console.log(res.data);
        if(res.data.length > 0){
         this.setState({PR: true});
-        this.repos += "API ";
+        this.repos += "AUTH ";
        } 
      });
     // API
