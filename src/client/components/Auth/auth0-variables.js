0fa558bbc9e02bbc80ae273ad394478491130f3e
Brandon Fizer <30492636+Track7Dev@users.noreply.github.com>
2018-02-22 19:50:49 -0500

    Updated

--- src/client/components/Auth/auth0-variables.js
+++ src/client/components/Auth/auth0-variables.js
@@ -1,5 +1,6 @@
+import {apiServerIP} from 'capstone-utils';
 export const AUTH_CONFIG = {
   domain: 'leveld.auth0.com',
   clientId: '59qiZ8qOFGyWC2d6ZJtK0v7ypEFV6KOs',
-  callbackUrl: 'http://auth.localhost.test:3002/login'
+  callbackUrl: `${apiServerIP}login`
 }
