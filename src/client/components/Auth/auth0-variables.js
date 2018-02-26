1774ada36cc18c51663d85cfc532c70625da6de9
Brandon Fizer <30492636+Track7Dev@users.noreply.github.com>
2018-02-26 04:39:34 -0500

    fixed callback from API -> Auth (Need a handle on the api for login so only that server has access to the auth and db server instead of auth0 -> Auth server

--- src/client/components/Auth/auth0-variables.js
+++ src/client/components/Auth/auth0-variables.js
@@ -1,6 +1,6 @@
-import {apiServerIP} from 'capstone-utils';
+import {authServerIP} from 'capstone-utils';
 export const AUTH_CONFIG = {
   domain: 'leveld.auth0.com',
   clientId: '59qiZ8qOFGyWC2d6ZJtK0v7ypEFV6KOs',
-  callbackUrl: `${apiServerIP}login`
+  callbackUrl: `${authServerIP}login`
 }
