1dc2a18b5fb7f51b91691603b15f43a16fe7c759
Brandon Fizer <30492636+Track7Dev@users.noreply.github.com>
2018-02-28 03:39:31 -0500

    reg upload

--- src/client/components/Pages/Registration.js
+++ src/client/components/Pages/Registration.js
@@ -68,8 +68,8 @@ class Registration extends Component {
       alert("Registration Complete");
       const file = new FormData();
       file.append('image',this.state.file);
-      axios.post(`${frontServerIP}upload`, file, {headers:{Authorization: 'Bearer ' + token}});
-      //window.location.replace('/');
+      axios.post(`${frontServerIP}upload`, file, {headers:{Authorization: 'Bearer ' + token}})
+      .then(() => window.location.replace('/'));
     })
     .catch((err) => {
       alert(err.response.data.message);
