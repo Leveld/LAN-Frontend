bc3ac46094c5d9a2292a03f42fe145a35f8ee166
Donnie Reese <donniereese@gmail.com>
2018-03-05 23:40:53 -0600

    testing .scss changes tracking

--- src/client/components/App/Footer.js
+++ src/client/components/App/Footer.js
@@ -3,31 +3,31 @@ import {connect} from 'react-redux';
 import {version} from '../../../server/config.json';
 
 class Footer extends Component {
-  
-  render(){
+
+  render() {
     let inDev = false;
-    process.env.NODE_ENV === "production" || process.env.PRODUCTION ? inDev = false : inDev = true; 
+    process.env.NODE_ENV === "production" || process.env.PRODUCTION ? inDev = false : inDev = true;
     return (
       <div className="App-footer-wrapper">
         {
-          inDev ? 
+          inDev ?
             <div className="App-footer-wrapper">
               <div className="App-footer-dev-indicator">
                 IN DEVELOPMENT
-              </div>        
+              </div>
               <div className="App-footer-wrapper-auth">
                 <div style={{color: 'white', marginRight: 5, height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}> {version} </div>
                   {
-                    this.props.authenticated ? 
-                      <div style={{background: 'green'}} className="App-footer-wrapper-auth-display">AUTHENTICATED</div> : 
+                    this.props.authenticated ?
+                      <div style={{background: 'green'}} className="App-footer-wrapper-auth-display">AUTHENTICATED</div> :
                       <div style={{background: 'red'}} className="App-footer-wrapper-auth-display"> NOT AUTHENTICATED</div>
                   }
               </div>
-            </div> 
+            </div>
           : null
         }
         <div className="App-footer-lambda" ><div className="App-footer-lambda-wrapper" >POWERED BY <div style={{color: 'black', display: 'block'}}>LAMBDASCHOOL</div></div></div>
-      
+
       </div>
     );
   }
