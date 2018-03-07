16e77658fcab9d1ed11e21d9258e6dc4fac20e4c
Brandon Fizer <30492636+Track7Dev@users.noreply.github.com>
2018-03-07 12:54:07 -0500

    view pro

--- src/client/components/App/Header.js
+++ src/client/components/App/Header.js
@@ -93,7 +93,7 @@ class Header extends Component {
           {this.props.authenticated ? <Link to="/" ><img  src={this.props.user.profilePicture ? this.props.user.profilePicture : 'images/noPhoto.jpg'} alt="Profile Pic" style={{height: '50px', width: '50px', borderRadius: '100%', marginLeft: 20}} /></Link> : <img  src={'images/logo/logo.png'} alt="Logo" style={{height: '90%', marginLeft: 20}}/>}      
         <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start'}}  className="App-header-logo" >
           <div className="App-header-username"> {this.props.user.name}</div>
-          {this.props.user.name ? <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}><div className="App-header-settings" onClick={() => this.props.authenticated ? this.props.toggleSettings() : null }>S</div><Link className="App-header-view" to={`/profile?id=${this.props.user._id}&type=${this.props.user.type}`}>View Profile</Link></div> : null}
+          {this.props.user.name && this.props.authenticated ? <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}><div className="App-header-settings" onClick={() => this.props.authenticated ? this.props.toggleSettings() : null }>S</div><Link className="App-header-view" to={`/profile?id=${this.props.user._id}&type=${this.props.user.type}`}>View Profile</Link></div> : null}
 
         </div>
         <div onClick={()=> console.log(this.state.data)} style={this.state.data.length > 0 ? { position: 'absolute', marginLeft: 5, padding: 5, borderRadius: 5,color:'white', background: 'red', fontSize: '0.7rem'} : {display: 'none'}}>PULL REQUEST FOUND <hr /> {this.state.repos}</div>
