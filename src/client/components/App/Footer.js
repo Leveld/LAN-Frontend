890a609d2e13ec186c2228cf81bab508ee350da3
Nick Costigan <nick.costigan@gmail.com>
2018-03-08 15:24:00 -0500

    more styling

--- src/client/components/App/Footer.js
+++ src/client/components/App/Footer.js
@@ -9,28 +9,25 @@ class Footer extends Component {
     process.env.NODE_ENV === 'production' || process.env.PRODUCTION ? inDev = false : inDev = true;
     return (
       <footer className="App-footer-wrapper">
-        {
+        {/*{
           inDev ? 
             <div style={{display: 'flex', flexDirection: 'row', height: '100%', flex: 1, justifyContent: 'flex-end'}}>
               <div className="App-footer-dev-indicator">
                 IN-DEV
               </div>        
               <div className="App-footer-wrapper-auth">
-                <div className='app-version'> {version} </div>
-                  {
+                <div className='app-version'> v1.0 </div>
+                  
                     this.props.authenticated ?
                       <div className="util-bg-success App-footer-wrapper-auth-display">AUTHENTICATED</div> :
                       <div className="util-bg-success App-footer-wrapper-auth-display"> NOT AUTHENTICATED</div>
-                  }
+                 
               </div>
             </div>
           : null
-        }
-        <div className="App-footer-lambda" >
-            <div className="App-footer-lambda-wrapper" >
-                POWERED BY <div>LAMBDASCHOOL</div>
-            </div>
-        </div>
+        }*/}
+        <div>Powered By LambdaSchool</div>
+        <div className="footer-version">v1.0</div>
       </footer>
     );
   }
