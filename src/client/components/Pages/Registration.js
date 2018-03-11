885a727c5aa8ee0632a234a5837e9faa1779d504
Brandon Fizer <30492636+Track7Dev@users.noreply.github.com>
2018-03-11 00:58:09 -0500

    tmp rmv reg upload

--- src/client/components/Pages/Registration.js
+++ src/client/components/Pages/Registration.js
@@ -129,8 +129,8 @@ class Registration extends Component {
               <label style={{fontSize:'10px'}}>Input Tax ID or upload any other relevant document to verify your business</label>
               <input name="taxID" onChange={(e) => this.setState({taxID: e.target.value})} placeholder="Tax ID" />
               {/*File Img Preview*/}
-              <img src={this.state.filePreview ? this.state.filePreview.result : null} width="30%"/>
-              {/* <div style={{background: 'red',  margin: '1%', overflow: 'hidden', position:'relative', display: 'flex', justifyContent: 'center', alignItems:'center'}}>
+              {/*<img src={this.state.filePreview ? this.state.filePreview.result : null} width="30%"/>
+               <div style={{background: 'red',  margin: '1%', overflow: 'hidden', position:'relative', display: 'flex', justifyContent: 'center', alignItems:'center'}}>
                 <input onChange={(e) => this.setImg(e)} type="file" style={{width: '100%', opacity: 0, height: '100%', display: 'flex', position: 'absolute'}}/>
                 <div style={{width: '100px', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>UPLOAD</div>
               </div> */}
