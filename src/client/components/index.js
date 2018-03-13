3d1fae75462bdbd3a7e89d5f86288096445136e8
Brandon Fizer <30492636+Track7Dev@users.noreply.github.com>
2018-03-13 02:40:49 -0400

    Update index.js

--- src/client/components/index.js
+++ src/client/components/index.js
@@ -1,18 +1,18 @@
 import InfoGraphicList from './InfoGraphics/InfoGraphicList';
 import InfoGraphicDisplay from './InfoGraphics/InfoGraphicDisplay';
 import InfoGraphic from './InfoGraphics/InfoGraphic';
-import MemberHome from '../components/Pages/Member_Home';
-import Profile from '../components/Pages/Profile';
+import MemberHome from './Pages/Member_Home';
+import Profile from './Pages/Profile';
 import Home from './Pages/Home';
 import Stats from '../components/Stats/Stats';
-import Header from '../components/App/Header';
-import Footer from '../components/App/Footer';
-import Registration from '../components/Pages/Registration';
-import Messages from '../components/Pages/Messages';
-import SettingsSidebar from '../components/Settings/Settings_Sidebar';
-import CampaignAdd from '../components/Campaigns/CampaignAdd';
-import Error from '../components/Pages/Error';
-import Search from '../components/Search/Search';
+import Header from './App/Header';
+import Footer from './App/Footer';
+import Registration from './Pages/Registration';
+import Messages from './Pages/Messages';
+import SettingsSidebar from './Settings/Settings_Sidebar';
+import CampaignAdd from './Campaigns/CampaignAdd';
+import Error from './Pages/Error';
+import Search from './Search/Search';
 
 export {
   InfoGraphicList,
