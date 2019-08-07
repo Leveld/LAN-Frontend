7feab9a7bf127cbf838a62a09ab05fe162fd55da
Brandon Fizer <brandon.m.fizer@gmail.com>
2019-08-07 17:56:21 -0400

    Update auth0-variables.js

--- src/client/components/Auth/auth0-variables.js
+++ src/client/components/Auth/auth0-variables.js
@@ -2,5 +2,5 @@ import {authServerIP} from 'capstone-utils';
 export const AUTH_CONFIG = {
   domain: 'leveld.auth0.com',
   clientId: '59qiZ8qOFGyWC2d6ZJtK0v7ypEFV6KOs',
-  callbackUrl: `https://leveld-auth.herokuapp.com/login`
+  callbackUrl: `${authServerIP}login`
 }
