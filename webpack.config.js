5d40c0f230dcfb707c7ca7ccfb3985d6bcfe2ff3
Brandon Fizer <brandon.m.fizer@gmail.com>
2019-08-07 21:19:45 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = [
     },
     plugins: clientLoaders.concat([
       new MiniCssExtractPlugin({
-        filename: '[name].[contenthash].css'
+        filename: '/css/styles.css'
       })
     ]),
     module: {
