c5785c7fc90191e7052fdf3825ace3469b918915
Brandon Fizer <brandon.m.fizer@gmail.com>
2019-08-08 09:57:14 -0400

    Update auth0-variables.js

--- src/client/components/Auth/auth0-variables.js
+++ src/client/components/Auth/auth0-variables.js
@@ -1,4 +1,5 @@
 import {authServerIP} from 'capstone-utils';
+authServerIP = 'https://leveld-auth.herokuapp.com';
 export const AUTH_CONFIG = {
   domain: 'leveld.auth0.com',
   clientId: '59qiZ8qOFGyWC2d6ZJtK0v7ypEFV6KOs',
