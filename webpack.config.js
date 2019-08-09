62761b9a5926912319eea0dfc24aa4b9c473ec1e
Brandon Fizer <brandon.m.fizer@gmail.com>
2019-08-09 02:40:31 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -30,8 +30,8 @@ module.exports = [
       new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // all options are optional
-      filename: 'styles.css',
-      chunkFilename: 'styles.css',
+      filename: '[name].css',
+      chunkFilename: '[id].css',
       ignoreOrder: false, // Enable to remove warnings about conflicting order
     }),
     
@@ -45,7 +45,7 @@ rules: [
             options: {
               // you can specify a publicPath here
               // by default it uses publicPath in webpackOptions.output
-              publicPath: '/public',
+              publicPath: '/',
               hmr: process.env.NODE_ENV === 'development',
             },
           },
